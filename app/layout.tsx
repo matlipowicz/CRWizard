1b21b285aaf1b181b3c7509dc4b560c1dc6027fb
Mateusz Lipowicz <lipusek1997@gmail.com>
2024-08-24 19:42:43 +0200

    config: eslint rules setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import './globals.css';
+import React from 'react';
+import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-import type { Metadata } from 'next';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
