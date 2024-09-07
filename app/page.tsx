0d084752def7094b236045900277da8b0b28885b
Mateusz Lipowicz <lipusek1997@gmail.com>
2024-09-07 14:39:16 +0200

    config: icon setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 import { Icons } from '@/components/theme/icons/icons';
-=======
->>>>>>> main
 import { Example } from '@/components/ThemeExample';
 
 export default function Home() {
   return (
     <main>
       <Example />
-<<<<<<< HEAD
       <Icons.logo />
-=======
->>>>>>> main
     </main>
   );
 }
